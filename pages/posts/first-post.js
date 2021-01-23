1f29893834612c4f15791b05eaabce137a66f0f1
Leonardo Luz Fernandes <leonardo.l.fernandes@gmail.com>
2021-01-22 21:03:07 -0300

    adding robots and head

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,15 @@
+import Head from "next/head";
 import Link from "next/link";
 
 export default function FirstPost(){
     return ( 
         <>
+            <Head>
+                <title>First Post</title>
+                <meta property="og:title" content="First Post" key="title"/>
+            </Head>
             <h1>First Post</h1>
             <h2>
-                // link component enable client-side navigation between two pages
                 <Link href="/">
                     <a>Back to home</a>
                 </Link>
